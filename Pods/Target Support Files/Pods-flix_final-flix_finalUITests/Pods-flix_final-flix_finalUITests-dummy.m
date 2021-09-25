02a64ad749f1823a50bcf62f9a81efec3397c789
Alem <ans682@nyu.edu>
2021-09-24 21:19:30 -0400

    finished the app

---
+++ Pods/Target Support Files/Pods-flix_final-flix_finalUITests/Pods-flix_final-flix_finalUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_flix_final_flix_finalUITests : NSObject
+@end
+@implementation PodsDummy_Pods_flix_final_flix_finalUITests
+@end
---
+++ flix_final/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  flix_final
+//
+//  Created by Alem Shaimardanov on 24/09/2021.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
